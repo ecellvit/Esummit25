327028746fbe05990a285351b0942d680537ad43
puniti-web <punitijodhwani29@gmail.com>
2025-01-15 20:28:42 +0530

    footer and marquee

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,17 @@
 // pages/index.tsx or any page
 import SessionWrapper from "@/components/SessionWrapper";
+import Footer from "@/components/footer";
+import Marq from "@/components/marquee";
 import NavBar from "@/components/navbar"
+import Marquee from "react-fast-marquee";
 
 export default function HomePage() {
   return (
     <div>
       <NavBar/>
+      <Marq/>
+      <Footer/>
+    
     </div>
   );
 }
