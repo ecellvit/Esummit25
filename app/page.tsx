fc84590511695c85d0384a91619674b12bb50d6a
Monish <monish-instinct@users.noreply.github.com>
2025-02-06 22:46:37 +0530

    registration button

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Footer from "@/components/footer";
 import Marq from "@/components/marquee";
 import NavBar from "@/components/navbar";
 import EventButtons from "@/components/EventButton";
+import RegistrationButtons from "@/components/registrationButton";
 import CustomCursor from "@/components/cursor";
 
 export default function HomePage() {
@@ -18,6 +19,15 @@ export default function HomePage() {
           4: "/events/event4", 
           5: "/events/event5", 
         }}
+      />
+      <RegistrationButtons
+       eventUrls={{
+        1: "/events/event1/Join_and_Create_Team", 
+        2: "/events/event2", 
+        3: "/events/event3", 
+        4: "/events/event4", 
+        5: "/events/event5", 
+      }}
       />
       <Marq speed1={100} speed2={50} speed3={75} speed4={120} />
       <Footer />
