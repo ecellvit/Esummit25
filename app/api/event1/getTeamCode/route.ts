3c78a12ef41adecf2d8945e950d8c5ed5936facc
DNahar74 <nahardarsh55@gmail.com>
2025-02-06 21:46:57 +0530

    changed deleteTeam, getTeamCode and removeMember route

--- app/api/event1/getTeamCode/route.ts
+++ app/api/event1/getTeamCode/route.ts
@@ -3,15 +3,20 @@ import { getServerSession } from "next-auth";
 import { Users } from "@/models/user.model";
 import { dbConnect } from "@/lib/dbConnect";
 import TeamModel from "@/models/event1/Team.model";
+import { authOptions } from "../../auth/[...nextauth]/route";
 
 export async function GET(req: NextRequest) {
   await dbConnect();
   try {
-    const url = new URL(req.url);
-    const userId = url.searchParams.get("userId");
+    const session = await getServerSession(authOptions);
+    const sessionUser = session?.user;
+    
+    if (!session || !sessionUser) {
+      return NextResponse.json({success: false, message: "User not authenticated"}, {status: 401});
+    }
 
     // Find the user by userId and populate event1TeamId to get the team information
-    const user = await Users.findById(userId).populate("event1TeamId");
+    const user = await Users.findOne({ email: sessionUser.email });
     if (!user || !user.event1TeamId) {
       return NextResponse.json(
         { message: "User has no team." },
@@ -23,7 +28,7 @@ export async function GET(req: NextRequest) {
     const userRole = user.event1TeamRole;
     if (userRole === 1) {
       // User is a member
-      return NextResponse.json({ message: "User is a member." });
+      return NextResponse.json({ message: "User is a member." }, { status: 409 });
     }
 
     // If the user is a leader, return the team details
