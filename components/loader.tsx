2851b1b9b16f2056c62f3211de9a7a7993abb552
KV225511 <krissh.verma2023@vitstudent.ac.in>
2025-02-11 00:32:52 +0530

    added the loading component

--- components/loader.tsx
+++ components/loader.tsx
@@ -1,12 +1,15 @@
 "use client";
 
 export default function Loader() {
-
   return (
-    <div className="flex justify-center items-center h-screen w-screen absolute z-10">
-      <div
-        className="rounded-full w-[50px] h-[50px]  animate-spin transition-all duration-500 ease-in-out border-red-500 border-t-4 border-l-4"
-      ></div>
+    <div className="flex justify-center items-center h-screen w-screen absolute z-10 bg-black bg-opacity-50">
+      <div className="relative flex items-center justify-center">
+        {/* Outer ring */}
+        <div className="absolute w-16 h-16 border-4 border-transparent border-t-blue-500 border-r-purple-500 rounded-full animate-spin"></div>
+
+        {/* Inner glow effect */}
+        <div className="w-10 h-10 bg-gradient-to-r from-blue-500 to-purple-500 rounded-full shadow-lg"></div>
+      </div>
     </div>
   );
 }
