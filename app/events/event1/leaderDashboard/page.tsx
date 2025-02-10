fb62f623fee79b9cda0c417e975b0192c0bea03e
KV225511 <krissh.verma2023@vitstudent.ac.in>
2025-02-11 00:48:36 +0530

    added the addMemberModalWindow to the leaderDashboard

--- app/events/event1/leaderDashboard/page.tsx
+++ app/events/event1/leaderDashboard/page.tsx
@@ -7,30 +7,19 @@ import toast, { Toaster } from "react-hot-toast";
 import Navbar from "@/components/navbar";
 import background from "/assets/bg.png";
 import background1 from "/assets/divbg.png";
-import picture from "@/assets/member.png"
+import picture from "@/assets/member.png";
 import { useSession } from "next-auth/react";
-
-
-type TeamMember = {
-  id: number;
-  uid: string; // MongoDB ObjectID
-  name: string;
-  regNo: string;
-  mobNo: string;
-  event1TeamRole?: number;
-};
+import AddMemberModalWindow from "@/components/AddMemberModalWindow";
 
 export default function Page() {
   const router = useRouter();
   const [teamName, setTeamName] = useState<string | null>(null);
-  const [teamMembers, setTeamMembers] = useState<TeamMember[]>([]);
+  const [teamMembers, setTeamMembers] = useState([]);
   const [loading, setLoading] = useState<boolean>(true);
   const [showModal, setShowModal] = useState<boolean>(false);
-  const [modalMemberIndex, setModalMemberIndex] = useState<number | null>(null);
   const [modalType, setModalType] = useState<string>("");
   const [teamCode, setTeamCode] = useState<string>("");
-  const [showLeaderModal, setShowLeaderModal] = useState<boolean>(false);
-  const [newLeaderId, setNewLeaderId] = useState<string | null>(null);
+  const [showAddMemberModal, setShowAddMemberModal] = useState<boolean>(false);
   const { data: session, update } = useSession();
 
   useEffect(() => {
@@ -41,21 +30,9 @@ export default function Page() {
     setLoading(true);
     try {
       const { data } = await axios.get("/api/event1/getTeamDetails");
-
       if (data?.teamMembersData?.length) {
         setTeamName(data.teamName);
-        const formattedMembers = data.teamMembersData.map((member: TeamMember, index: number) => ({
-          id: index, // Ensure each member has a unique ID
-          name: member.name,
-          regNo: member.regNo,
-          mobNo: member.mobNo,
-          event1TeamRole: member.event1TeamRole,
-        }));
-
-        setTeamMembers(formattedMembers);
-        console.log("Fetched and formatted members:", formattedMembers);
-        setTeamMembers(formattedMembers);
-        console.log("Fetched and formatted members:", formattedMembers);
+        setTeamMembers(data.teamMembersData);
       }
     } catch (error) {
       toast.error("An error occurred while fetching team data.");
@@ -64,124 +41,11 @@ export default function Page() {
     }
   };
 
-  const handleShowModal = (index: number | null = null, type: string = "") => {
-    setModalMemberIndex(index);
-    setModalType(type);
-    setShowModal(true);
-  };
-
-  const handleCloseModal = () => {
-    setModalMemberIndex(null);
-    setModalType("");
-    setShowModal(false);
-  };
-
-
-  const handleLeave = async () => {
-    const leader = teamMembers.find((member) => member.event1TeamRole === 0); // Find the current leader
-
-    if (!leader || !leader.uid) {
-      toast.error("Error: Leader not found.");
-      return;
-    }
-
-    // If there is only one member (leader), delete the team directly
-    if (teamMembers.length === 1) {
-      await leaveTeam(leader.uid, null); // Pass null for new leader as team will be deleted
-      return;
-    }
-
-    // If there are other members, show modal for leader selection
-    setShowLeaderModal(true);
-  };
-
-
-  const leaveTeam = async (leaderId: string, newLeaderId: string | null) => {
-    setLoading(true);
-    try {
-      const response = await axios.patch("/api/event1/leaveTeam", {
-        userId: leaderId,
-        newLeaderId, // Send new leader ID if applicable
-      });
-
-      if (response.status === 200) {
-        toast.success("You have left the team successfully.");
-        update({ ...session, user: { ...session?.user, event1TeamRole: null } });
-        router.push("/"); // Redirect user after leaving
-      } else {
-        toast.error(response.data.message || "Failed to leave the team.");
-      }
-    } catch (error: any) {
-      console.error("Error leaving team:", error);
-      toast.error(error.response?.data?.message || "An error occurred.");
-    } finally {
-      setLoading(false);
-      handleCloseModal();
-    }
-  };
-
-  const handleConfirmLeaderChange = async () => {
-    if (!newLeaderId) {
-      toast.error("Please select a new leader.");
-      return;
-    }
-
-    const leader = teamMembers.find((member) => member.event1TeamRole === 0);
-    if (!leader || !leader.uid) {
-      toast.error("Error: Leader not found.");
-      return;
-    }
-
-    await leaveTeam(leader.uid, newLeaderId);
-  };
-
-  const handleRemove = async () => {
-    if (modalMemberIndex === null) {
-      toast.error("Error: No team member selected.");
-      return;
-    }
-
-
-    const memberToRemove = teamMembers[modalMemberIndex]; // Find the member
-    const memberIdToRemove = memberToRemove.id; // Get ID safely
-    console.log('ripun', memberToRemove);
-
-    if (!memberIdToRemove) {
-      console.error("Error: Invalid memberIdToRemove", { modalMemberIndex, memberToRemove });
-      toast.error("Error: Member ID is invalid.");
-      return;
-    }
-
-    console.log(`Removing member ID: ${memberIdToRemove}`);
-
-    setLoading(true);
-    try {
-      const response = await axios.patch("/api/event1/removeMember", {
-        memberIdToRemove,
-      });
-
-
-      if (response.status === 200) {
-        toast.success("Team member removed successfully");
-        setTeamMembers((prev) => prev.filter((_, i) => i !== modalMemberIndex));
-      } else {
-        toast.error(response.data.message || "Failed to remove team member.");
-      }
-    } catch (error: any) {
-      console.error("Error removing team member:", error);
-      toast.error(error.response?.data?.message || "An error occurred.");
-    } finally {
-      setLoading(false);
-      handleCloseModal();
-    }
-  };
-
   const handleViewTeamCode = async () => {
     try {
       const response = await axios.get("/api/event1/getTeamCode");
       setTeamCode(response.data.teamCode);
-      setModalType("teamCode");
-      setShowModal(true);
+      setShowAddMemberModal(true);
     } catch (error) {
       toast.error("Could not fetch team code.");
     }
@@ -201,159 +65,65 @@ export default function Page() {
       ) : (
         <>
           <div
-            className=" min-h-screen w-full sm:w-3/4 lg:w-2/3 xl:w-1/2 flex flex-col items-center justify-start bg-cover bg-center p-4 rounded-lg "
-            style={{ backgroundImage: `url(${background1.src})`, backgroundSize: "cover", backgroundPosition: "center",backgroundColor: "rgba(255, 255, 255, 0.5)", }}
+            className="min-h-screen w-full sm:w-3/4 lg:w-2/3 xl:w-1/2 flex flex-col items-center justify-start bg-cover bg-center p-4 rounded-lg"
+            style={{ backgroundImage: `url(${background1.src})` }}
           >
-            <h1 className="text-2xl sm:text-3xl font-extrabold mb-4 text-center drop-shadow-lg text-black">
+            <h1 className="text-2xl sm:text-3xl font-extrabold mb-4 text-center text-black">
               {teamName || "Team Name Not Found"}
             </h1>
-
             <div className="grid grid-cols-1 sm:grid-cols-2 gap-6 w-full max-w-4xl px-8 py-10">
-
-
               {teamMembers.length > 0 ? (
                 teamMembers.map((member, index) => (
                   <div
-  key={member.id || index}
-  className="bg-[#141B2B] opacity-85 rounded-lg p-3 text-center shadow-lg transform hover:scale-105 transition-transform duration-300 flex flex-row items-center justify-between space-x-4"
-  style={{ backgroundImage: `url(${background.src})` }}
->
-  {/* Left Section - Member Details */}
-  <div className="relative z-10 flex-1 p-4 text-left">
-    <h2 className="text-lg font-bold mb-1 text-white">{member.name}</h2>
-    <p className="text-xs mb-1 text-white">Reg. No.: {member.regNo}</p>
-    <p className="text-xs text-white">Mobile No.: {member.mobNo}</p>
-    <h1 className="text-lg font-bold mb-1 text-white">
-       {member.event1TeamRole === 0 ? "LEADER" : "MEMBER"}
-    </h1>
-
-    <button
-      className="mt-3 rounded-3xl bg-gradient-to-r from-purple-500 to-blue-500 text-center w-[50vw] sm:w-[15vw] h-[5vh] hover:scale-110 active:scale-95 transition-transform ease-in-out duration-300"
-      onClick={() => {
-        handleShowModal(index, member.event1TeamRole === 0 ? "leave" : "remove");
-      }}
-    >
-      {member.event1TeamRole === 0 ? "Leave" : "Remove"}
-    </button>
-  </div>
-  {/* Right Section - Member Image */}
-  <div className="absolute right-0 top-0 w-1/2 h-full">
-    <Image
-      src={picture}
-      alt={`${member.name}'s profile`}
-      layout="fill" 
-      objectFit="cover" 
-      className="opacity-100"
-    />
-
-  </div>
-</div>
-
-                  // <div
-                  //   key={member.id || index}
-                  //   className="bg-[#141B2B] opacity-85 rounded-lg p-3 text-center shadow-lg transform hover:scale-105 transition-transform duration-300 flex flex-col items-center justify-between"
-                  //   style={{ backgroundImage: `url(${background.src})` }}
-                  // >
-                  //   <h2 className="text-lg font-bold mb-1 text-white">{member.name}</h2>
-                  //   <h2 className="text-lg font-bold mb-1 text-white"
-                  //   >
-                  //     Role: {member.event1TeamRole === 0 ? "Leader" : "Member"}
-                  //   </h2>
-                  //   <p className="text-xs mb-1 text-white">Reg. No.: {member.regNo}</p>
-                  //   <p className="text-xs text-white">Mobile No.: {member.mobNo}</p>
-
-                  //   <button
-                  //     className="mb-7 sm:landscape:w-[15vw] rounded-3xl bg-gradient-to-r from-purple-500 to-blue-500 text-center w-[50vw] h-[5vh] hover:scale-110 active:scale-95 transition-transform ease-in-out duration-300"
-                  //     onClick={() => {
-                  //       handleShowModal(index, member.event1TeamRole === 0 ? "leave" : "remove");
-                  //     }}
-                  //   >
-                  //     {member.event1TeamRole === 0 ? "Leave" : "Remove"}
-                  //   </button>
-                  // </div>
+                    key={index}
+                    className="bg-[#141B2B] opacity-85 rounded-lg p-3 text-center shadow-lg transform hover:scale-105 transition-transform duration-300 flex flex-row items-center justify-between space-x-4"
+                  >
+                    <div className="relative z-10 flex-1 p-4 text-left">
+                      <h2 className="text-lg font-bold mb-1 text-white">
+                        {member.name}
+                      </h2>
+                      <p className="text-xs mb-1 text-white">
+                        Reg. No.: {member.regNo}
+                      </p>
+                      <p className="text-xs text-white">
+                        Mobile No.: {member.mobNo}
+                      </p>
+                      <h1 className="text-lg font-bold mb-1 text-white">
+                        {member.event1TeamRole === 0 ? "LEADER" : "MEMBER"}
+                      </h1>
+                    </div>
+                    <div className="absolute right-0 top-0 w-1/2 h-full">
+                      <Image
+                        src={picture}
+                        alt={`${member.name}'s profile`}
+                        layout="fill"
+                        objectFit="cover"
+                        className="opacity-100"
+                      />
+                    </div>
+                  </div>
                 ))
               ) : (
                 <p className="text-white">No team members found.</p>
               )}
             </div>
-
-            {/* <button className="btn-primary mt-4" onClick={handleViewTeamCode}>
-              Add Member
-            </button> */}
             <div className="flex flex-col sm:flex-row items-center justify-center gap-4 mt-6">
-  {/* Add Member Button */}
-  <button 
-    className="btn-primary bg-gre-500 text-white px-4 py-2 rounded-md hover:scale-105 transition-transform"
-    onClick={handleViewTeamCode}
-  >
-    Add Member
-  </button>
-
-  {/* Leave Team Button (Only for Leader) */}
-  {teamMembers.some(member => member.event1TeamRole === 0) && (
-    <button
-      className="btn-secondary bg-red-500 text-white px-4 py-2 rounded-md hover:scale-105 transition-transform"
-      onClick={() => handleShowModal(null, "leave")}
-    >
-      Leave Team
-    </button>
-  )}
-</div>
-            
-
-            {/* Modal for Remove */}
-            {showModal && modalType === "remove" && modalMemberIndex !== null && (
-              <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center">
-                <div className="bg-white p-5 rounded-md text-center">
-                  <p className="mb-4">Are you sure you want to remove this user?</p>
-                  <div className="flex justify-around">
-                    <button onClick={handleRemove} className="bg-green-500 text-white px-4 py-2 rounded-md">
-                      Yes
-                    </button>
-                    <button onClick={handleCloseModal} className="bg-red-500 text-white px-4 py-2 rounded-md">
-                      No
-                    </button>
-                  </div>
-                </div>
-              </div>
-            )}
-
-            {/* Modal for Leave */}
-            {showModal && modalType === "leave" && modalMemberIndex !== null && (
-              <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center">
-                <div className="bg-white p-5 rounded-md text-center">
-                  <p className="mb-4">Are you sure you want to leave the team?</p>
-                  <div className="flex justify-around">
-                    <button onClick={handleRemove} className="bg-green-500 text-white px-4 py-2 rounded-md">
-                      Yes
-                    </button>
-                    <button onClick={handleCloseModal} className="bg-red-500 text-white px-4 py-2 rounded-md">
-                      No
-                    </button>
-                  </div>
-                </div>
-              </div>
-            )}
-
-            {/* Modal for Team Code */}
-            {showModal && modalType === "teamCode" && (
-              <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
-                <div className="bg-white p-4 rounded-lg shadow-lg">
-                  <h2 className="text-xl font-bold mb-4">Team Code</h2>
-                  <p className="mb-4">{teamCode}</p>
-                  <button className="btn-secondary" onClick={handleCloseModal}>
-                    Close
-                  </button>
-                </div>
-              </div>
-            )}
-            
+              <button
+                className="bg-purple-500 text-white px-4 py-2 rounded-md hover:scale-105 transition-transform"
+                onClick={handleViewTeamCode}
+              >
+                Add Member
+              </button>
+            </div>
           </div>
         </>
-        
       )}
-      
-
+      {showAddMemberModal && (
+        <AddMemberModalWindow
+          teamName={teamName || ""}
+          onClose={() => setShowAddMemberModal(false)}
+        />
+      )}
       <Toaster />
     </div>
   );
