18db5ec88297f039f973929f04e9583553f69055
kv8gh <vyaskaran1409@gmail.com>
2025-02-14 02:28:48 +0530

    making registration number to uppercase

--- app/userDetails/page.tsx
+++ app/userDetails/page.tsx
@@ -63,7 +63,7 @@ export default function UserDetail() {
     const { name, value } = e.target;
     setFormData({
       ...formData,
-      [name]: value,
+      [name]: name==="regNo"?value.toUpperCase():value,
     });
   };
 
@@ -220,70 +220,3 @@ export default function UserDetail() {
     </div>
   );
 }
-//   return (
-//     <div className="bg-cover bg-center bg-no-repeat"
-//     style={{
-//       backgroundImage: `url(${background.src})`,
-//       backgroundSize: 'cover',
-//     }}>
-//       {loading && (
-//         <div className="flex justify-center items-center fixed inset-0 bg-black bg-opacity-50 z-50">
-//           <div className="text-white text-2xl">Loading...</div>
-//         </div>
-//       )}
-//       <div className="flex justify-center items-center lg:grid-cols-3 min-h-screen gap-4 p-4 md:p-8 lg:p-10">
-//         <div className="col-span-1 sm:col-span-2 lg:col-span-2 flex items-center justify-center p-4 lg:p-8 bg-cover bg-center sm:border border-gray-600 rounded-3xl overflow-hidden">
-//           <div className="w-full max-w-md lg:max-w-lg">
-//             <form
-//               onSubmit={handleSubmit}
-//               className="flex flex-col gap-10 bg-transparent p-2 rounded-lg shadow-none min-w-[full] min-h-[full] text-3xl"
-//             >
-//               <div className="flex flex-col gap-2">
-//                 <input
-//                   placeholder=" Full Name"
-//                   type="text"
-//                   id="name"
-//                   name="name"
-//                   value={formData.name}
-//                   onChange={handleChange}
-//                   onKeyPress={handleKeyPress}
-//                   className="border rounded-md text-2xl text-black border-gray-300 focus:ring-blue-200 focus:outline-none focus:ring-2 p-2"
-//                 />
-//               </div>
-//               <div className="flex flex-col gap-2">
-//                 <input
-//                   placeholder=" Registration Number"
-//                   type="text"
-//                   id="regNo"
-//                   name="regNo"
-//                   value={formData.regNo}
-//                   onChange={handleChange}
-//                   className="border rounded-md text-2xl text-black border-gray-300 focus:ring-blue-200 focus:outline-none focus:ring-2 p-2"
-//                 />
-//               </div>
-//               <div className="flex flex-col gap-2">
-//                 <input
-//                   placeholder=" Phone Number"
-//                   type="tel"
-//                   id="number"
-//                   name="number"
-//                   value={formData.number}
-//                   onChange={handleChange}
-//                   onWheel={(e) => e.currentTarget.blur()}
-//                   className="border rounded-md text-2xl text-black border-gray-300 focus:ring-blue-200 focus:outline-none focus:ring-2 p-2"
-//                 />
-//               </div>
-//               <button
-//                 type="submit"
-//                 className="p-2 rounded-3xl bg-gradient-to-r from-purple-500 to-blue-500 text-white text-2xl hover:text-black active:transform transition duration-200 w-full h-auto text-center font-sans"
-//               >
-//                 Register
-//               </button>
-//             </form>
-//           </div>
-//         </div>
-//       </div>
-//       <Toaster />
-//     </div>
-//   );
-// }
