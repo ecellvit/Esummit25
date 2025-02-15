9d84491f41e78ff1b9e28c9a57e8f357a7c3dae4
ripunsethia <ripun.sethia2023@vitstudent.ac.in>
2025-02-15 21:36:39 +0530

    landingPage motion added

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import SessionWrapper from "@/components/SessionWrapper";
 import Footer from "@/components/footer";
 import Marq from "@/components/marquee";
@@ -10,26 +11,34 @@ import MeetOurSpeakers from "@/components/meetOurSpeakers";
 import Sponsors from "@/components/sponsors";
 import FaqContent from "@/components/faq";
 import EsummitMotion from "@/components/EsummitMotion";
+import IdeateComponent from "@/components/iiaMotion";
+import React, { useState, useEffect } from "react";
+
 export default function HomePage() {
+  const [ideateCompleted, setIdeateCompleted] = useState(false);
   return (
     <div>
     <CustomCursor /> 
-      <SignInBtn/>
-      <RegistrationButtons
-       eventUrls={{
-        1: "/events/event1/createTeam", 
-        2: "/events/event2", 
-        3: "/events/event3", 
-        4: "/events/event4", 
-        5: "/events/event5", 
-      }}
-      />
-      {/* <Marq speed1={100} speed2={50} speed3={75} speed4={120} /> */}
-      <EsummitMotion/>
-      <MeetOurSpeakers />
-      <Sponsors />
-      <FaqContent/>
-      <Footer /> 
+    {!ideateCompleted && <IdeateComponent onComplete={() => setIdeateCompleted(true)} />}
+    {ideateCompleted && (
+        <>
+          <SignInBtn />
+          <RegistrationButtons
+            eventUrls={{
+              1: "/events/event1/createTeam",
+              2: "/events/event2",
+              3: "/events/event3",
+              4: "/events/event4",
+              5: "/events/event5",
+            }}
+          />
+          <EsummitMotion />
+          <MeetOurSpeakers />
+          <Sponsors />
+          <FaqContent />
+          <Footer />
+        </>
+      )}
     </div>
   );
 }
