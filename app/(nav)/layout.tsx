1fde5131f69bfcfb4155ea72c97c32e6716e0851
KV225511 <krissh.verma2023@vitstudent.ac.in>
2025-02-17 00:21:13 +0530

    changes made

--- app/(nav)/layout.tsx
+++ app/(nav)/layout.tsx
@@ -3,10 +3,11 @@ import Navbar from "@/components/navbar";
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      
-        <Navbar />
+      <div className="absolute top-0 z-50">
+        <Navbar/>
+      </div>
         {children}
-     
     </>
+     
   );
 }
