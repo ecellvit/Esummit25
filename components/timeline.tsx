bad68ebb14b79749b533090c0770474b47c1dd42
kv8gh <vyaskaran1409@gmail.com>
2025-02-19 02:30:59 +0530

    timeline change

--- components/timeline.tsx
+++ components/timeline.tsx
@@ -165,7 +165,7 @@ export default function Schedule() {
         }
         await update({ ...session, user: { ...session?.user, events: newUserEvents } });
 
-        router.push(event === 1 ? `/events/event${event}/createTeam` : "/#timeline");
+        router.push(event === 1 ? `/events/event${event}/createTeam` : "/MySchedule");
       }
     } catch (error) {
       const axiosError = error as AxiosError;
