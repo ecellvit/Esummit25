ea83111d4fd0c85a23164e35f6f1ad7d7caec680
bharatwalejain <aarjavkjain2@gmail.com>
2025-02-22 17:56:02 +0530

    fix: round1 navbar

--- components/round1/navbar.tsx
+++ components/round1/navbar.tsx
@@ -31,19 +31,19 @@ const Navbar = () => {
 
   return (
     <nav
-      className="fixed top-5 md:top-8 left-[50%] w-[80vw] lg:w-[70vw] h-[8vh]
+      className="fixed top-5 md:top-8 left-[50%] w-[80vw] lg:w-[70vw] h-[9vh]
         rounded-lg md:rounded-2xl border-2 border-red-700 shadow-xl z-20
         transition-transform duration-300 transform -translate-x-1/2 
         bg-black bg-opacity-75 text-md lg:text-lg"
     >
-      <div className="flex items-center justify-between h-full px-8">
-        <Link 
-          href="./dashboard" 
-          className="flex items-center gap-3 text-white hover:text-red-300 transition-colors duration-300"
-        >
-          <Home className="w-6 h-6" />
-          <span className="font-medium">Dashboard</span>
-        </Link>
+      <div className="flex items-center justify-between h-full px-4 lg:px-8">
+        <div className="h-full flex items-center">
+          <Image
+            src={logo}
+            alt="Logo"
+            className="h-6 lg:h-8 w-auto object-contain"
+          />
+        </div>
         <div className="flex items-center gap-2 text-white hover:text-red-300 px-4 py-2 rounded-lg">
           <Timer className="w-5 h-5" />
           <span className="font-bold">{formatTime(timeLeft)}</span>
@@ -55,13 +55,13 @@ const Navbar = () => {
           <Wallet className="w-5 h-5" />
           <span className="font-bold">₹ {walletBalance.toLocaleString()}</span>
         </div>
-        <div className="h-full flex items-center">
-          <Image
-            src={logo}
-            alt="Logo"
-            className="h-6 lg:h-8 w-auto object-contain"
-          />
-        </div>
+        <Link 
+          href="./dashboard" 
+          className="flex items-center gap-3 text-white hover:text-red-300 transition-colors duration-300"
+        >
+          <Home className="w-6 h-6" />
+          <span className="font-medium">Dashboard</span>
+        </Link>
       </div>
     </nav>
   );
