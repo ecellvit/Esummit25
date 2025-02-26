d147526a2604591876410e2f936533f41f634062
ripunsethia <ripun.sethia2023@vitstudent.ac.in>
2025-02-26 19:18:21 +0530

    fix

--- models/event1/round2Island.model.ts
+++ models/event1/round2Island.model.ts
@@ -2,67 +2,52 @@ import mongoose, { Document, ObjectId, Schema } from "mongoose";
 
 export interface round2Island extends Document {
     teamLeaderId: ObjectId;
-    teamLeaderName: string;
     teamLeaderEmail: string;
-    teamName: string;
-    wallet: number;
+    cost: number;
     startTime: Date;
+    endTime: Date;
+    modeOfTransport: number;
     totalQuantity: number;
-    islandElement: number[][];
-    elementBatch1: number[];
-    elementBatch2: number[];
-    elementBatch3: number[];
-    insuranceType: number[];
-
+    elementQuantity: number[];
 }
 
 const TeamSchema: Schema<round2Island> = new Schema(
-{
-    teamLeaderId: {
-        type: Schema.Types.ObjectId,
-        ref: "Users",
-        required: [true, "Team leader is required"],
-    },
-    teamLeaderName: {
-        type: String,
-        required: [true, "Team leader name is required"],
-    },
-    teamLeaderEmail: {
-        type: String,
-        required: [true, "Team leader email is required"],
-        unique: true,
-    },
-    teamName:{
-        type: String,
-        required: [true, "Team name is required"],
-    },
-    wallet:
     {
-        type: Number,
-        required: [true, "Wallet is required"],
+        teamLeaderId: {
+            type: Schema.Types.ObjectId,
+            ref: "Users",
+            required: [true, "Team leader is required"],
+        },
+        teamLeaderEmail: {
+            type: String,
+            required: [true, "Team leader email is required"],
+            unique: true,
+        },
+        cost: {
+            type: Number,
+        },
+        startTime: {
+            type: Date,
+            default: Date.now,
+        },
+        endTime: {
+            type: Date,
+            default: Date.now,
+        },
+        modeOfTransport: {
+            type: Number,
+        },
+        totalQuantity: {
+            type: Number,
+        },
+        elementQuantity: {
+            type: [Number],
+        },
+        
     },
-    portfolio:
-    {
-        type: [Number],
-        required: [true, "Portfolio is required"],
-    },
-    page:
-    {
-        type: Number,
-        required: [true, "Page is required"],
-    },
-    startTime: {
-        type: Date,
-        default: Date.now,
-    },
-    totalQuantity: {
-        type: Number,
-    },
-    
-},
-    { collection: "TeamsEvent1Round1" }
+    { collection: "TeamsEvent1Round2" }
 );
 
-const TeamModelRound1 = (mongoose.models.TeamsEvent1Round1 as mongoose.Model<round2Island>) || (mongoose.model<round2Island>("TeamsEvent1Round1", TeamSchema));
+const TeamModelRound2 = (mongoose.models.TeamsEvent1Round2 as mongoose.Model<round2Island>) || (mongoose.model<round2Island>("TeamsEvent1Round2", TeamSchema));
 
-export default TeamModelRound1;
+export default TeamModelRound2;
