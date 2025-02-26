e27f961380f058e01225b6c6f269e56048688476
ripunsethia <ripun.sethia2023@vitstudent.ac.in>
2025-02-26 18:59:06 +0530

    schema added

--- models/event1/round2Island.model.ts
+++ models/event1/round2Island.model.ts
@@ -1,26 +1,11 @@
 import mongoose, { Document, ObjectId, Schema } from "mongoose";
 
-export interface round1Qualified extends Document {
+export interface round2Island extends Document {
     teamLeaderId: ObjectId;
     teamLeaderName: string;
     teamLeaderEmail: string;
     teamName: string;
-    primaryElement: number;
-    secondaryElement: number;
-    lease1Element: number;
-    lease2Element: number;
-    primaryRate: number;
-    secondaryRate: number;
-    lease1Rate: number;
-    lease2Rate: number;
-    primaryStartTime: Date;
-    secondaryStartTime: Date;
-    lease1StartTime: Date;
-    lease2StartTime: Date;
-    hasUpgraded: boolean;
     wallet: number;
-    portfolio: number[];
-    page: number;
     startTime: Date;
     totalQuantity: number;
     islandElement: number[][];
@@ -31,7 +16,7 @@ export interface round1Qualified extends Document {
 
 }
 
-const TeamSchema: Schema<round1Qualified> = new Schema(
+const TeamSchema: Schema<round2Island> = new Schema(
 {
     teamLeaderId: {
         type: Schema.Types.ObjectId,
@@ -50,46 +35,6 @@ const TeamSchema: Schema<round1Qualified> = new Schema(
     teamName:{
         type: String,
         required: [true, "Team name is required"],
-    },
-    primaryElement: {
-        type: Number,
-    },
-    secondaryElement: {
-        type: Number,
-    },
-    lease1Element: {
-        type: Number,
-    },
-    lease2Element: {
-        type: Number,
-    },
-    primaryRate: {
-        type: Number,
-    },
-    secondaryRate: {
-        type: Number,
-    },
-    lease1Rate: {
-        type: Number,
-    },
-    lease2Rate: {
-        type: Number,
-    },
-    primaryStartTime: {
-        type: Date,
-    },
-    secondaryStartTime: {
-        type: Date,
-    },
-    lease1StartTime: {
-        type: Date,
-    },
-    lease2StartTime: {
-        type: Date,
-    },
-    hasUpgraded: {
-        type: Boolean,
-        default: false,
     },
     wallet:
     {
@@ -118,6 +63,6 @@ const TeamSchema: Schema<round1Qualified> = new Schema(
     { collection: "TeamsEvent1Round1" }
 );
 
-const TeamModelRound1 = (mongoose.models.TeamsEvent1Round1 as mongoose.Model<round1Qualified>) || (mongoose.model<round1Qualified>("TeamsEvent1Round1", TeamSchema));
+const TeamModelRound1 = (mongoose.models.TeamsEvent1Round1 as mongoose.Model<round2Island>) || (mongoose.model<round2Island>("TeamsEvent1Round1", TeamSchema));
 
 export default TeamModelRound1;
