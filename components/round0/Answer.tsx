39938f8b4a8886edd3128a53b59e106d1a98e745
KV225511 <krissh.verma2023@vitstudent.ac.in>
2025-02-26 09:36:05 +0530

    changes made to round0

--- components/round0/Answer.tsx
+++ components/round0/Answer.tsx
@@ -3,294 +3,122 @@ import React, { useEffect, useState } from "react";
 import questions from "@/constant/round0/questions.json";
 
 interface Props {
-  questionCategory: "easy" | "medium" | "hard" ;
+  questionCategory: "easy" | "medium" | "hard";
   questionNumber: number;
   setFinalAnswer: (answer: string[]) => void;
   finalAnswer: string[];
   changeOption: boolean;
 }
 
-interface Questions {
-  [key: string]: {
-    [key: number]: {
-      q: {
-        questionType: "single" | "multiple";
-      };
-      ans: {
-        optionsType: "text" | "image";
-        optionsContent: {
-          [key: string]: string;
-        };
-      };
-    };
-  };
-}
-
-const AnswerForQualifier: React.FC<Props> = (props) => {
+const AnswerForQualifier: React.FC<Props> = ({
+  questionCategory,
+  questionNumber,
+  setFinalAnswer,
+  finalAnswer,
+  changeOption,
+}) => {
   const [selectedOptions, setSelectedOptions] = useState<{
     [key: number]: string[];
   }>({});
 
   useEffect(() => {
     setSelectedOptions({});
-  }, [props.questionNumber]);
-
-  useEffect(() => {
-    setSelectedOptions({});
-  }, [props.changeOption]);
+  }, [questionNumber, changeOption]);
 
   const storeAnswer = (ele: string) => {
-    props.setFinalAnswer([ele]);
+    setFinalAnswer([ele]);
   };
 
   const handleOptionChange = (questionId: number, option: string) => {
     const isSelected = selectedOptions[questionId]?.includes(option);
+    const questionType = questions[questionCategory]?.[questionId]?.q
+      .questionType as "single" | "multiple";
 
     setSelectedOptions((prevOptions) => ({
       ...prevOptions,
-      [questionId]: getUpdatedOptions(
-        prevOptions[questionId] || [],
-        option,
-        questions[props.questionCategory]?.[questionId]?.q.questionType as "single" | "multiple",
-        isSelected
-      ),
+      [questionId]:
+        questionType === "single"
+          ? [option]
+          : isSelected
+          ? prevOptions[questionId].filter(
+              (selectedOption) => selectedOption !== option
+            )
+          : [...(prevOptions[questionId] || []), option],
     }));
   };
 
-  const getUpdatedOptions = (
-    prevOptions: string[],
-    option: string,
-    optionType: "single" | "multiple",
-    isSelected: boolean
-  ) => {
-    if (optionType === "single") {
-      return [option];
-    } else {
-      return isSelected
-        ? prevOptions.filter((selectedOption) => selectedOption !== option)
-        : [...prevOptions, option];
-    }
+  const renderOptions = () => {
+    const questionData = questions[questionCategory]?.[questionNumber];
+    if (!questionData) return null;
+
+    return Object.entries(questionData.ans.optionsContent).map(
+      ([key, value]) => (
+        <div
+          key={key}
+          onClick={() => {
+            handleOptionChange(questionNumber, key);
+            storeAnswer(key);
+          }}
+        >
+          {questionData.ans.optionsType === "text" ? (
+            <label>
+              <input
+                type="radio"
+                onChange={() => {
+                  handleOptionChange(questionNumber, key);
+                  storeAnswer(key);
+                }}
+                checked={
+                  selectedOptions[questionNumber]?.includes(key) || false
+                }
+                className="mr-2 select-none"
+              />
+              {value}
+            </label>
+          ) : (
+            <div className="flex">
+              <input
+                type="radio"
+                onChange={() => {
+                  handleOptionChange(questionNumber, key);
+                  storeAnswer(key);
+                }}
+                checked={
+                  selectedOptions[questionNumber]?.includes(key) || false
+                }
+                className="mr-2"
+              />
+              <img
+                src={value}
+                className="w-[300px] h-auto"
+                alt="answer option"
+              />
+            </div>
+          )}
+        </div>
+      )
+    );
   };
 
   return (
     <main className="text-black">
       <section className="flex flex-col justify-center items-center text-black p-4 select-none">
-        {props.questionCategory === "easy" && (
-          <div className="flex flex-col items-center text-xl">
-            <h2 className="font-bold mb-4 text-green-600">Easy Question</h2>
-            <ul className="grid grid-cols-2 gap-8">
-              {questions["easy"] &&
-                questions["easy"][props.questionNumber] &&
-                questions["easy"][props.questionNumber]?.ans.optionsContent &&
-                Object.keys(
-                  questions["easy"][props.questionNumber]?.ans.optionsContent
-                ).map((ele) => {
-                  return (
-                    <div
-                      key={ele}
-                      onClick={() => {
-                        handleOptionChange(props.questionNumber, ele);
-                        storeAnswer(ele);
-                      }}
-                    >
-                      {questions["easy"][props.questionNumber]?.ans
-                        .optionsType === "text" ? (
-                        <div key={ele}>
-                          <label>
-                            <input
-                              type="radio"
-                              onChange={() => {
-                                handleOptionChange(props.questionNumber, ele);
-                                storeAnswer(ele);
-                              }}
-                              checked={
-                                selectedOptions[props.questionNumber]?.includes(
-                                  ele
-                                ) || false
-                              }
-                              className="mr-2 select-none"
-                            />
-                            {
-                              questions["easy"][props.questionNumber]?.ans
-                                .optionsContent[ele]
-                            }
-                          </label>
-                        </div>
-                      ) : (
-                        <div className="flex" key={ele}>
-                          <input
-                            type="radio"
-                            onChange={() => {
-                              handleOptionChange(props.questionNumber, ele);
-                              storeAnswer(ele);
-                            }}
-                            checked={
-                              selectedOptions[props.questionNumber]?.includes(
-                                ele
-                              ) || false
-                            }
-                            className="mr-2"
-                          />
-                          <img
-                            src={
-                              questions["easy"][props.questionNumber]?.ans
-                                .optionsContent[ele]
-                            }
-                            className="w-[300px] h-auto"
-                            alt="answer image"
-                          />
-                        </div>
-                      )}
-                    </div>
-                  );
-                })}
-            </ul>
-          </div>
-        )}
-        {props.questionCategory === "medium" && (
-          <div className="flex flex-col items-center text-xl">
-            <h2 className="font-bold mb-4 text-yellow-600">Medium Question</h2>
-            <ul className="grid grid-cols-2 gap-8">
-              {questions["medium"] &&
-                questions["medium"][props.questionNumber] &&
-                questions["medium"][props.questionNumber]?.ans.optionsContent &&
-                Object.keys(
-                  questions["medium"][props.questionNumber]?.ans.optionsContent
-                ).map((ele) => {
-                  return (
-                    <div
-                      key={ele}
-                      onClick={() => {
-                        handleOptionChange(props.questionNumber, ele);
-                        storeAnswer(ele);
-                      }}
-                    >
-                      {questions["medium"][props.questionNumber]?.ans
-                        .optionsType === "text" ? (
-                        <div key={ele}>
-                          <label>
-                            <input
-                              type="radio"
-                              onChange={() => {
-                                handleOptionChange(props.questionNumber, ele);
-                                storeAnswer(ele);
-                              }}
-                              checked={
-                                selectedOptions[props.questionNumber]?.includes(
-                                  ele
-                                ) || false
-                              }
-                              className="mr-2 select-none"
-                            />
-                            {
-                              questions["medium"][props.questionNumber]?.ans
-                                .optionsContent[ele]
-                            }
-                          </label>
-                        </div>
-                      ) : (
-                        <div className="flex" key={ele}>
-                          <input
-                            type="radio"
-                            onChange={() => {
-                              handleOptionChange(props.questionNumber, ele);
-                              storeAnswer(ele);
-                            }}
-                            checked={
-                              selectedOptions[props.questionNumber]?.includes(
-                                ele
-                              ) || false
-                            }
-                            className="mr-2"
-                          />
-                          <img
-                            src={
-                              questions["medium"][props.questionNumber]?.ans
-                                .optionsContent[ele]
-                            }
-                            className="w-[300px] h-auto"
-                            alt="answer image"
-                          />
-                        </div>
-                      )}
-                    </div>
-                  );
-                })}
-            </ul>
-          </div>
-        )}
-        {props.questionCategory === "hard" && (
-          <div className="flex flex-col items-center text-xl">
-            <h2 className="font-bold mb-4 text-yellow-600">Medium Question</h2>
-            <ul className="grid grid-cols-2 gap-8">
-              {questions["hard"] &&
-                questions["hard"][props.questionNumber] &&
-                questions["hard"][props.questionNumber]?.ans.optionsContent &&
-                Object.keys(
-                  questions["hard"][props.questionNumber]?.ans.optionsContent
-                ).map((ele) => {
-                  return (
-                    <div
-                      key={ele}
-                      onClick={() => {
-                        handleOptionChange(props.questionNumber, ele);
-                        storeAnswer(ele);
-                      }}
-                    >
-                      {questions["hard"][props.questionNumber]?.ans
-                        .optionsType === "text" ? (
-                        <div key={ele}>
-                          <label>
-                            <input
-                              type="radio"
-                              onChange={() => {
-                                handleOptionChange(props.questionNumber, ele);
-                                storeAnswer(ele);
-                              }}
-                              checked={
-                                selectedOptions[props.questionNumber]?.includes(
-                                  ele
-                                ) || false
-                              }
-                              className="mr-2 select-none"
-                            />
-                            {
-                              questions["hard"][props.questionNumber]?.ans
-                                .optionsContent[ele]
-                            }
-                          </label>
-                        </div>
-                      ) : (
-                        <div className="flex" key={ele}>
-                          <input
-                            type="radio"
-                            onChange={() => {
-                              handleOptionChange(props.questionNumber, ele);
-                              storeAnswer(ele);
-                            }}
-                            checked={
-                              selectedOptions[props.questionNumber]?.includes(
-                                ele
-                              ) || false
-                            }
-                            className="mr-2"
-                          />
-                          <img
-                            src={
-                              questions["hard"][props.questionNumber]?.ans
-                                .optionsContent[ele]
-                            }
-                            className="w-[300px] h-auto"
-                            alt="answer image"
-                          />
-                        </div>
-                      )}
-                    </div>
-                  );
-                })}
-            </ul>
-          </div>
-        )}
+        <div className="flex flex-col items-center text-xl">
+          <h2
+            className={`font-bold mb-4 ${
+              questionCategory === "easy"
+                ? "text-green-600"
+                : questionCategory === "medium"
+                ? "text-yellow-600"
+                : "text-red-600"
+            }`}
+          >
+            {questionCategory.charAt(0).toUpperCase() +
+              questionCategory.slice(1)}{" "}
+            Question
+          </h2>
+          <ul className="grid grid-cols-2 gap-8">{renderOptions()}</ul>
+        </div>
       </section>
     </main>
   );
--- components/round0/instructions.tsx
+++ components/round0/instructions.tsx
@@ -1,179 +1,88 @@
 "use client";
+
 import { useSession } from "next-auth/react";
 import { useEffect, useState } from "react";
 import toast, { Toaster } from "react-hot-toast";
 import CountdownTimer from "./counter1";
-// import LoadingIcons from "react-loading-icons";
-// import formLinks from "@/constant/round0/form";
 
 const Instructions: React.FC = () => {
   const [buttonEnabled, setButtonEnabled] = useState<boolean>(false);
   const [loading, setLoading] = useState<boolean>(false);
-  // const [timeRemaining, setTimeRemaining] = useState<number>(60);
-  const { data: session, status } = useSession();
-  const targetDate = new Date(2024, 9, 3, 21, 0, 0).toISOString();
+  const { data: session } = useSession();
+  const targetDate: string = new Date(2024, 9, 3, 21, 0, 0).toISOString();
 
-
-
-
-  /* const calculateTimeRemaining = () => {
-    const now = new Date().getTime();
-
-    const targetTime = new Date(
-      2024,
-      3,
-      time.quizStartTime.day,
-      time.quizStartTime.hour,
-      time.quizStartTime.minute,
-      time.quizStartTime.second
-    );
-    const timeDiff = targetTime - now;
-
-    if (timeDiff <= 0) {
-      // Target date has passed
-      setButtonEnabled(true);
-      return { minutes: "00", seconds: "00", hours: "00" };
-    }
-
-    if (Math.floor(timeDiff / 1000) <= 0) {
-      console.log("asdf");
-    }
-
-    const hours = Math.floor(
-      (timeDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-    );
-    const minutes = Math.floor((timeDiff % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((timeDiff % (1000 * 60)) / 1000);
-    return {
-      hours: hours.toString().padStart(2, "0"),
-      minutes: minutes.toString().padStart(2, "0"),
-      seconds: seconds.toString().padStart(2, "0"),
-    };
-  };
-
-  const [timeRemaining, setTimeRemaining] = useState(calculateTimeRemaining);
-
-  useEffect(() => {
-    // if early then disable button
-
-    const intervalId = setInterval(() => {
-      setTimeRemaining(calculateTimeRemaining);
-    }, 1000);
-
-    // Clear the interval when the component unmounts
-    return () => clearInterval(intervalId);
-  }, []); */
-
-  // useEffect(() => {
-  //   if (timeRemaining > 0) {
-  //     const timerInterval = setInterval(() => {
-  //       setTimeRemaining((prev) => prev - 1);
-  //     }, 1000);
-
-  //     return () => clearInterval(timerInterval); // Clear interval when component unmounts
-  //   } else if (timeRemaining == 0) {
-  //     setButtonEnabled(true); // Enable button when timer reaches 0
-  //   }
-  // }, [timeRemaining]);
-
-  const startQuiz = () => {
-    // e.preventDefault();
-    // console.log("inside");
-    setLoading(true);
-    fetch("/api/round0/startQuiz", {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: session?.accessTokenBackend ? `Bearer ${session.accessTokenBackend}` : '',
-        "Access-Control-Allow-Origin": "*",
-      }
-    })
-      .then((res) => {
-        console.log("inside response", res);
-        console.log(res.status);
-        if (res.status === 200) {
-          console.log("quizStartingNow.");
-          location.reload();
-        } else if (res.status === 403) {
-          toast.error("Quiz has not started yet");
-        } else {
-          toast.error("too late");
-        }
-        setLoading(false);
-        console.log(res.status);
-        return res.json();
-      })
-      .then((data) => {
-        console.log(data);
-      })
-      .catch((err) => {
-        console.log(err);
+  const startQuiz = async (): Promise<void> => {
+    try {
+      setLoading(true);
+      const res = await fetch("/api/round0/startQuiz", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: session?.accessTokenBackend
+            ? `Bearer ${session.accessTokenBackend}`
+            : "",
+          "Access-Control-Allow-Origin": "*",
+        },
       });
+
+      if (res.status === 200) {
+        toast.success("Quiz is starting now!");
+        location.reload();
+      } else if (res.status === 403) {
+        toast.error("Quiz has not started yet.");
+      } else {
+        toast.error("Too late to start.");
+      }
+    } catch (error) {
+      toast.error("An error occurred while starting the quiz.");
+      console.error("Start Quiz Error:", error);
+    } finally {
+      setLoading(false);
+    }
   };
 
   return (
     <main className="min-h-[100vh] text-black flex flex-col items-center">
       <div className="flex flex-col items-start w-[90vw] px-8 py-4 border rounded-xl m-2 text-xl">
         <div className="px-[43%]">
-          <CountdownTimer targetDate={targetDate}/>
+          <CountdownTimer targetDate={targetDate} />
         </div>
+        <h1 className="font-bold">
+          Welcome to the Qualifying Round of FuturePreneurs 10.0!
+        </h1>
         <p>
-          <h1><b>Welcome to the Qualifying round of FuturePrenuers 10.0!</b></h1>
-          <br/>
-          This qualifying round will evaluate your entrepreneurial knowledge, and business understanding. Your performance on this quiz will determine your eligibility to advance to the next round. 
-
+          This round evaluates your entrepreneurial knowledge and business
+          understanding. Your performance in this quiz determines eligibility
+          for the next round.
         </p>
-        <br />
-        <p>
-          Read the following instructions carefully to ensure a smooth and
-          successful completion of the quiz.
-          
-        </p>
-        <br/>
-        <h1><b>General Instructions:-</b></h1>
-        <ul className="list-inside list-disc">
-          <li>
-          All the participants will be given 30 minutes for the completion of the quiz.
-          </li>
-          <li>
-          A timer will be displayed at the top of the screen.              
-          </li>
-          <li>
-          The quiz can only be submitted after completion, otherwise, it will auto-submit after 30 minutes.
-          </li>
-          <li>
-          There is <span className="text-black-600">no</span> negative marking.
-          </li>
-          <li>
-          You can skip and navigate through the questions. 
-          </li>
-          <br/>
-          <h1><b>Section division:</b></h1>
-          <li>
-            The quiz consists of a total of 25 questions and is divided into THREE sections.
-          </li>
-          <li>
-          Sections are categorized by the level of difficulty: Easy, Medium, and Hard.
-          </li>
-          <li>Section one consists of 8 questions, each of 3 marks.</li>
-          <li>Section two consists of 12 questions, each of 4 marks.</li>
-          <li>Section three consists of 5 questions, each of 5 marks.</li>
+        <h2 className="font-bold mt-4">General Instructions:</h2>
+        <ul className="list-disc list-inside">
+          <li>All participants have 30 minutes to complete the quiz.</li>
+          <li>A countdown timer is displayed at the top of the screen.</li>
+          <li>The quiz auto-submits after 30 minutes.</li>
+          <li>No negative marking.</li>
+          <li>You can navigate and skip questions.</li>
         </ul>
-        <br/>
-        <h2><b>Best of luck to all the participants!</b></h2>
+
+        <h2 className="font-bold mt-4">Section Breakdown:</h2>
+        <ul className="list-disc list-inside">
+          <li>Total: 25 questions across three sections.</li>
+          <li>Easy: 8 questions (3 marks each).</li>
+          <li>Medium: 12 questions (4 marks each).</li>
+          <li>Hard: 5 questions (5 marks each).</li>
+        </ul>
+
+        <h2 className="font-bold mt-4">Best of luck to all participants!</h2>
       </div>
-      <div>
-        <button
-          className={`px-4 py-2 rounded-full text-white bg-gradient-to-r from-purple-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none m-4 w-full h-12 flex items-center justify-center font-bold hover:opacity-80 hover:cursor-pointer`}
-          onClick={() => startQuiz()}
-        >
-          {/* {loading ? <LoadingIcons.Oval height={"20px"} /> : "Start Quiz"} */}
-          {loading ? "Loading..." : "Start Quiz"}
-        </button>
-        <div className="my-4">
-          {/* <p className="text-lg">Time remaining: <span className="text-red-500">{`${Math.floor(timeRemaining / 60)}:${(timeRemaining % 60).toString().padStart(2, "0")}`}</span></p> */}
-        </div>
-      </div>
+
+      <button
+        className="px-4 py-2 rounded-full text-white bg-gradient-to-r from-purple-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none m-4 w-full h-12 flex items-center justify-center font-bold hover:opacity-80 hover:cursor-pointer"
+        onClick={startQuiz}
+        disabled={loading}
+      >
+        {loading ? "Loading..." : "Start Quiz"}
+      </button>
+
       <Toaster />
     </main>
   );
