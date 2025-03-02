2d3082a6c7b883d804e8257257e28a9ab145e1ce
DNahar74 <nahardarsh55@gmail.com>
2025-03-03 01:03:56 +0530

    changed marketPrice formula

--- utils/calculateMarketPrice.ts
+++ utils/calculateMarketPrice.ts
@@ -1,5 +1,5 @@
 const calculateMarketPrice = (basePrice: number, teams: number): number => {
-  const mp = basePrice + (0.4-teams/40)*175
+  const mp = basePrice + (0.6-teams/40)*175
   return mp;
 }
 
