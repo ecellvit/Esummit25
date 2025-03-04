abcdc85e2c46f1ec46a62751aecf3809165cab36
bharatwalejain <aarjavkjain2@gmail.com>
2025-03-04 16:03:25 +0530

    fix: sponsor link

--- components/sponsors.tsx
+++ components/sponsors.tsx
@@ -2,6 +2,7 @@
 import React, { useEffect, useRef } from "react";
 import { gsap } from "gsap";
 import Image from "next/image";
+import Link from "next/link";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import s1 from "@/assets/sponsor1.svg";
 import s2 from "@/assets/sponsor2.svg";
@@ -53,11 +54,20 @@ const Sponsors: React.FC = () => {
         className="absolute top-[50vh] min-h-[70vh] w-full flex items-center justify-center rounded-t-3xl bg-white z-10"
       >
         <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-6 p-7 md:p-3 lg:p-0 w-[90%] mx-auto">
-          {[s1, s2, s3, s4, s5].map((sponsor, index) => (
-            <div key={index} className="flex justify-center items-center">
-              <Image src={sponsor} alt={`Sponsor ${index + 1}`} className="w-full h-auto object-cover"/>
-            </div>
-          ))}
+            {[s1, s2, s3, s4, s5].map((sponsor, index) => {
+            const sponsorLinks = [
+              "https://www.pizzahut.co.in",
+              "https://bingosnacks.com",
+              "https://jugnoo.io",
+              "https://campusexpress.org.in",
+              "http://www.ambalalrealestate.com"
+            ];
+            return (
+              <Link key={index} href={sponsorLinks[index]} target="_blank" rel="noopener noreferrer" className="flex justify-center items-center">
+                <Image src={sponsor} alt={`Sponsor ${index + 1}`} className="w-full h-auto object-cover"/>
+              </Link>
+            );
+            })}
         </div>
       </div>  
     </section>
